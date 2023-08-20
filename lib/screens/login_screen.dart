4a687031b25d823f5bbc260bf57d2597db350b4f
SRenuuu <sandulrenuja@gmail.com>
2023-08-20 09:38:26 +0530

    Refactor code in welcome, login and main screens

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -20,29 +20,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: SizedBox(
-          height: 40,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Text("Don't have an account?",
-                  style: TextStyle(
-                      color: CosmixColor.white,
-                      fontSize: 14,
-                      fontWeight: FontWeight.w400)),
-              const SizedBox(
-                height: 4.0,
-              ),
-              GestureDetector(
-                child: const Text("Sign up",
-                    style: TextStyle(
-                        color: CosmixColor.primaryColor,
-                        fontSize: 15,
-                        fontWeight: FontWeight.w600)),
-                onTap: () {},
-              )
-            ],
-          )),
+      floatingActionButton: _bottomContent(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: DecoratedBox(
         decoration: const BoxDecoration(
@@ -55,88 +33,117 @@ class _LoginScreenState extends State<LoginScreen> {
               image: AssetImage("assets/images/backgrounds/bg-3.webp"),
               fit: BoxFit.cover),
         ),
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                GlassCard(
-                  blur: 4,
-                  type: CardType.dark,
-                  borderRadius: 16,
-                  height: 336,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 32.0),
-                    child: Column(
-                      children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 40, bottom: 36),
-                          child: Text(
-                            "Sign in",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              letterSpacing: -0.5,
-                              fontSize: 32,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
+        child: _mainContent(),
+      ),
+    );
+  }
+
+  Widget _bottomContent() {
+    return SizedBox(
+        height: 40,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text("Don't have an account?",
+                style: TextStyle(
+                    color: CosmixColor.white,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400)),
+            const SizedBox(
+              height: 4.0,
+            ),
+            GestureDetector(
+              child: const Text("Sign up",
+                  style: TextStyle(
+                      color: CosmixColor.primaryColor,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w600)),
+              onTap: () {},
+            )
+          ],
+        ));
+  }
+
+  Widget _mainContent() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GlassCard(
+              blur: 4,
+              type: CardType.dark,
+              borderRadius: 16,
+              height: 336,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0),
+                child: Column(
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.only(top: 40, bottom: 36),
+                      child: Text(
+                        "Sign in",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          letterSpacing: -0.5,
+                          fontSize: 32,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
                         ),
-                        InputField(
-                            labelText: "Email",
-                            controller: emailController,
-                            leadingIcon: const Icon(Icons.person_2_outlined)),
-                        const SizedBox(height: 20.0),
-                        InputField(
-                            labelText: "Password",
-                            controller: emailController,
-                            leadingIcon:
-                                const Icon(Icons.lock_outline_rounded)),
-                        const SizedBox(height: 36.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text("Having trouble signing in?",
-                                style: TextStyle(
-                                    color: CosmixColor.white,
-                                    fontSize: 13,
-                                    fontWeight: FontWeight.w400)),
-                            const SizedBox(
-                              width: 4.0,
-                            ),
-                            GestureDetector(
-                              child: const Text("Get help",
-                                  style: TextStyle(
-                                      color: CosmixColor.primaryColor,
-                                      fontSize: 13,
-                                      fontWeight: FontWeight.w600)),
-                              onTap: () {},
-                            ),
-                          ],
+                      ),
+                    ),
+                    InputField(
+                        labelText: "Email",
+                        controller: emailController,
+                        leadingIcon: const Icon(Icons.person_2_outlined)),
+                    const SizedBox(height: 20.0),
+                    InputField(
+                        labelText: "Password",
+                        controller: emailController,
+                        leadingIcon: const Icon(Icons.lock_outline_rounded)),
+                    const SizedBox(height: 36.0),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text("Having trouble signing in?",
+                            style: TextStyle(
+                                color: CosmixColor.white,
+                                fontSize: 13,
+                                fontWeight: FontWeight.w400)),
+                        const SizedBox(
+                          width: 4.0,
+                        ),
+                        GestureDetector(
+                          child: const Text("Get help",
+                              style: TextStyle(
+                                  color: CosmixColor.primaryColor,
+                                  fontSize: 13,
+                                  fontWeight: FontWeight.w600)),
+                          onTap: () {},
                         ),
                       ],
                     ),
-                  ),
+                  ],
                 ),
-                const SizedBox(height: 24.0),
-                GlassButton(
-                  onPressed: () {
-                    print("clicked");
-                  },
-                  buttonText: "Login",
-                  type: ButtonType.primaryColor,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600,
-                  rightIcon: const Icon(
-                    Icons.arrow_forward_ios_rounded,
-                    color: Colors.white,
-                    size: 18,
-                  ),
-                ),
-              ],
+              ),
             ),
-          ),
+            const SizedBox(height: 24.0),
+            GlassButton(
+              onPressed: () {
+                print("clicked");
+              },
+              buttonText: "Login",
+              type: ButtonType.primaryColor,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              rightIcon: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                color: Colors.white,
+                size: 18,
+              ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -23,91 +23,107 @@ class WelcomeScreen extends StatelessWidget {
               image: AssetImage("assets/images/backgrounds/bg-1.webp"),
               fit: BoxFit.cover),
         ),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: _mainContent(),
+      ),
+    );
+  }
+
+  Widget _mainContent() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 32.0),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 70),
+              child: Image(
+                  width: 240,
+                  image: AssetImage("assets/images/other/cosmix-logo.png"),
+                  fit: BoxFit.cover),
+            ),
+            Column(
               children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 70),
-                  child: Image(
-                      width: 240,
-                      image: AssetImage("assets/images/other/cosmix-logo.png"),
-                      fit: BoxFit.cover),
+                _headlineText(),
+                const SizedBox(
+                  height: 40.0,
                 ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      width: double.infinity,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Your Inter-planetary",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              letterSpacing: -0.5,
-                              fontSize: 30,
-                              fontWeight: FontWeight.w400,
-                              color: Colors.white,
-                            ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Text(
-                            "Travel Partner",
-                            textAlign: TextAlign.left,
-                            style: TextStyle(
-                              letterSpacing: -0.5,
-                              fontSize: 48,
-                              fontWeight: FontWeight.w600,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40.0,
-                    ),
-                    GlassButton(
-                      onPressed: () {
-                        Get.to(() => const LoginScreen());
-                      },
-                      buttonText: "Get Started",
-                      type: ButtonType.primary,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      rightIcon: const Icon(
-                        Icons.arrow_forward_ios_rounded,
-                        color: Colors.white,
-                        size: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 24.0,
-                    ),
-                    GlassButton(
-                      onPressed: () {
-                        if (kDebugMode) {
-                          print("Explore clicked");
-                        }
-                      },
-                      buttonText: "Explore",
-                      type: ButtonType.secondary,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                    ),
-                    const SizedBox(
-                      height: 32.0,
-                    ),
-                  ],
+                _ctaButttons(),
+                const SizedBox(
+                  height: 32.0,
                 ),
               ],
             ),
-          ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _headlineText() {
+    return const SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Your Inter-planetary",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              letterSpacing: -0.5,
+              fontSize: 30,
+              fontWeight: FontWeight.w400,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(height: 8.0),
+          Text(
+            "Travel Partner",
+            textAlign: TextAlign.left,
+            style: TextStyle(
+              letterSpacing: -0.5,
+              fontSize: 48,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _ctaButttons() {
+    return Column(
+      children: [
+        GlassButton(
+          onPressed: () {
+            Get.to(() => const LoginScreen());
+          },
+          buttonText: "Get Started",
+          type: ButtonType.primary,
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          rightIcon: const Icon(
+            Icons.arrow_forward_ios_rounded,
+            color: Colors.white,
+            size: 18,
+          ),
+        ),
+        const SizedBox(
+          height: 24.0,
+        ),
+        GlassButton(
+          onPressed: () {
+            if (kDebugMode) {
+              print("Explore clicked");
+            }
+          },
+          buttonText: "Explore",
+          type: ButtonType.secondary,
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+      ],
     );
   }
 }
