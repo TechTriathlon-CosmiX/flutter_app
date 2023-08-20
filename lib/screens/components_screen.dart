910eb33f7c06fa96b82318ca5da8967ca1e46d3d
harini-udeshika <84621255+harini-udeshika@users.noreply.github.com>
2023-08-20 08:33:32 +0530

    added app components

--- lib/screens/components_screen.dart
+++ lib/screens/components_screen.dart
@@ -1,8 +1,10 @@
 import 'package:CosmiX/screens/buttons_screen.dart';
 import 'package:CosmiX/screens/from_to_card_screen.dart';
 import 'package:CosmiX/screens/inputs_screen.dart';
+import 'package:CosmiX/screens/my_trips_screen.dart';
 import 'package:CosmiX/screens/passenger_input_screen.dart';
 import 'package:CosmiX/screens/selectable_planet_screent.dart';
+import 'package:CosmiX/screens/space_lines.dart';
 import 'package:CosmiX/theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -22,47 +24,61 @@ class ComponentsScreen extends StatelessWidget {
       // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 36.0),
-        child: Center(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "Buttons",
-                onPressed: () => Get.to(() => const ButtonsScreen())),
-            const SizedBox(height: 24),
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "Cards",
-                onPressed: () => Get.to(() => const CardsScreen())),
-            const SizedBox(height: 24),
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "Inputs",
-                onPressed: () => Get.to(() => InputsScreen())),
-            const SizedBox(height: 24),
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "Planets",
-                onPressed: () => Get.to(() => SelectablePlanetsScreen())),
-            const SizedBox(height: 24),
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "From To Card",
-                onPressed: () => Get.to(() => const FromToCardScreen())),
-            const SizedBox(height: 24),
-            Button(
-                rightIcon: const Icon(Icons.arrow_forward_ios),
-                type: ButtonType.primary,
-                buttonText: "Search Filter Page",
-                onPressed: () => Get.to(() => PassengerInputScreen())),
-          ],
-        )),
+        child: SingleChildScrollView(
+          child: Center(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Buttons",
+                  onPressed: () => Get.to(() => const ButtonsScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Cards",
+                  onPressed: () => Get.to(() => const CardsScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Inputs",
+                  onPressed: () => Get.to(() => InputsScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Planets",
+                  onPressed: () => Get.to(() => SelectablePlanetsScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "From To Card",
+                  onPressed: () => Get.to(() => const FromToCardScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Search Filter Page",
+                  onPressed: () => Get.to(() => PassengerInputScreen())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "My Trips",
+                  onPressed: () => Get.to(() => MyTrips())),
+              const SizedBox(height: 24),
+              Button(
+                  rightIcon: const Icon(Icons.arrow_forward_ios),
+                  type: ButtonType.primary,
+                  buttonText: "Space Lines",
+                  onPressed: () => Get.to(() => SpaceLines())),
+            ],
+          )),
+        ),
       ),
     );
   }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-import 'package:CosmiX/screens/buttons_screen.dart';
-import 'package:CosmiX/screens/from_to_card_screen.dart';
-import 'package:CosmiX/screens/my_trips_screen.dart';
-import 'package:CosmiX/screens/passenger_input_screen.dart';
-import 'package:CosmiX/screens/selectable_planet_screent.dart';
-import 'package:CosmiX/screens/inputs_screen.dart';
-import 'package:CosmiX/screens/space_lines.dart';
-=======
->>>>>>> df77a3837047610528838aa0bbdf7dcfad1a9f98
 import 'package:CosmiX/theme/colors.dart';
 import 'package:flutter/material.dart';
 
